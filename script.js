891a5a1cc0e60e78448895d25e9d9b4dbb392b63
SamKinnon <garysamjones@gmail.com>
2023-09-04 12:13:57 +0200

    Javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+const accesKey = "bQK3RgfjJ9GidYVkBuz_um_b3HGwnA_DjEyple9wrVk"
+
+const formElement = document.querySelector("form")
+const inputElement = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-nore-button")
+
+let inputData = ""
+let page = 1;
+
+function searchImages(){
+    inputData = inputElement.value;
+    cons
+}
